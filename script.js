a1992e32620dadfc304a0d164c9fed61bb2e49a1
Simon Levey <lev_47@hotmail.com>
2022-02-28 15:17:10 +0800

    random password generates based on fields chosen by user

--- script.js
+++ script.js
@@ -26,16 +26,24 @@ setPasswordLengthSpan();
 
 });
 
+const lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbersSet = "1234567890";
+const symbolsSet = "!#$%&'()*+,-./:;<=>?@][_`{|}~";
+
+
+
 // When I click on the generate button
 generateBtn.addEventListener('click', function(event){
 
-  console.log(event);
 
 //The app will ask my password length 
   const passwordLength = Number (inputPasswordLength.value);
   console.log(passwordLength);
 
 //It will ask if I want to use (done in html): 
+
+let charset = "";
 
 // lowercase
 const wantsLowercase = inputLowercase.checked;
@@ -46,20 +54,39 @@ const wantsSymbols = inputSymbols.checked;
 // numbers
 const wantsNumbers = inputNumbers.checked;
 
-console.log(wantsLowercase, wantsUppercase, wantsSymbols, wantsNumbers);
-
-
-
-
-
-
 
 //in my password
 
 
 //Once I have selected all the options
 
+if(wantsLowercase){
+  charset = charset + lowercaseSet;
+}
+if(wantsUppercase){
+  charset = charset + uppercaseSet;
+}
+if(wantsNumbers){
+  charset = charset + numbersSet;
+}
+if(wantsSymbols){
+  charset = charset + symbolsSet;
+}
 //Then the app should generate a password based on options
+
+let password = ""
+
+//loop for password length times,
+for (let index = 0; index < passwordLength; index++) {
+//for each iteration,
+//we will generate a random characted from the charset chosen
+const randomIndex = Math.floor(Math.random() * charset.length);
+
+const randomChar = charset[randomIndex];
+//then we will add this random char to accumulator
+  password = password + randomChar;
+}
+
 
 //Then show the generated password in text area
 
