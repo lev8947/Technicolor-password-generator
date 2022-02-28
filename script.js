e8cfac7e6bb832082629b9cbcc89d0dda8b34741
Simon Levey <lev_47@hotmail.com>
2022-02-28 09:47:35 +0800

    added checked boxes and password length bar

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const generateBtn = document.querySelector("#generate");
+const inputPasswordLength = document.getElementById("input-length");
+const spanPasswordLength = document.getElementById("span-length-display");
 
-  passwordText.value = password;
+setPasswordLengthSpan();
 
-}
+function setPasswordLengthSpan(){
+    //grab the value of input
+  const passwordLength = inputPasswordLength.value
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  //change the text content of span
+  spanPasswordLength.textContent = passwordLength;
+};
+//when I change the slider
+inputPasswordLength.addEventListener('input', function(event){
+
+  //the span password length should be updated
+setPasswordLengthSpan();
+
+});
+
+// When I click on the generate button
+generateBtn.addEventListener('click', function(event){
+
+//The app will ask my password length 
+
+//It will ask if I want to use:
+
+// lowercase
+
+// uppercase
+
+// symbols
+
+// numbers
+
+//in my password
+
+
+//Once I have selected all the options
+
+//Then the app should generate a password
+
+//Then show the generated password in text area
+
+})
